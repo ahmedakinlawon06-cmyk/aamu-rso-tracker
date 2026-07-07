5f1efac7ce47597fb1d0f8e42acb7424e56bf40b
ahmedakinlawon06-cmyk <ahmedakinlawon06@gmail.com>
2026-07-06 21:42:53 -0500

    fix Netlify function - use https module

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,6 +1,20 @@
+const https = require("https");
+
 const RESEND_KEY = "re_fiD3XzdL_KbPbAmfA7RaAyCXeuTmp6Gxv";
 
 exports.handler = async function(event) {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+      body: ""
+    };
+  }
+
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
@@ -8,26 +22,40 @@ exports.handler = async function(event) {
   try {
     const { to, subject, html } = JSON.parse(event.body);
 
-    const res = await fetch("https://api.resend.com/emails", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + RESEND_KEY,
-      },
-      body: JSON.stringify({
-        from: "Pre-Alumni Association <noreply@prealumni.org>",
-        to: [to],
-        subject,
-        html
-      })
+    const payload = JSON.stringify({
+      from: "Pre-Alumni Association <noreply@prealumni.org>",
+      to: [to],
+      subject,
+      html
     });
 
-    const data = await res.json();
+    const result = await new Promise((resolve, reject) => {
+      const req = https.request({
+        hostname: "api.resend.com",
+        path: "/emails",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + RESEND_KEY,
+          "Content-Length": Buffer.byteLength(payload)
+        }
+      }, (res) => {
+        let data = "";
+        res.on("data", chunk => data += chunk);
+        res.on("end", () => resolve({ status: res.statusCode, body: data }));
+      });
+      req.on("error", reject);
+      req.write(payload);
+      req.end();
+    });
 
     return {
-      statusCode: res.ok ? 200 : 400,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify(data)
+      statusCode: result.status,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: result.body
     };
   } catch (e) {
     return {
